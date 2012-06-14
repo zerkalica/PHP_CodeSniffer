952b8871a36dcabee2fd4a23751c1c508ee2e177
Sergey Yuferev <s.yuferev@boombate.com>
2012-06-14 20:40:45 +0400

    updated

--- CodeSniffer/Standards/PEAR/Sniffs/Functions/FunctionDeclarationSniff.php
+++ CodeSniffer/Standards/PEAR/Sniffs/Functions/FunctionDeclarationSniff.php
@@ -141,7 +141,7 @@ class PEAR_Sniffs_Functions_FunctionDeclarationSniff implements PHP_CodeSniffer_
                 }
 
                 if ($expectedIndent !== $foundIndent) {
-                    $error = 'xMulti-line function declaration not indented correctly; expected %s spaces but found %s';
+                    $error = 'Multi-line function declaration not indented correctly; expected %s spaces but found %s';
                     $data  = array(
                               $expectedIndent,
                               $foundIndent,
